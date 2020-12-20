71b38655fae55badcbc2dc902a559041f0047ce3
Carlos <carlosegutierrez99@gmail.com>
2020-12-19 21:57:01 -0500

    primer archivo en subirse al repositorio

---
+++ ondas.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy	
+x= numpy.linspace(0,2*pi,100)
+
+
+y=((2*a*l**2*(-2)*(-1)**n)/(n*numpy.pi)**3)*(numpy.sin((n*numpy.pi*x)/l)
+#narray= (2,4,6,8)
+
+#plt.plot(x,y)
+#print(x)
+#print(/n*4)
+#print(y)
+print{4}
